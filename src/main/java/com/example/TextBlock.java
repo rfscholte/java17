130daa086b88520bf2fef6f39bc22c6d790860d0
rfscholte <rfscholte@apache.org>
2022-03-28 21:12:14 +0200

    Opschonen pom.xml Textblock voorbeeld uitbreiden met XML en JSon Eyecandy: ParameterizedTest verwijderen test-prefix (redundant, junit3 stijl) + record is geen keyword

--- src/main/java/com/example/TextBlock.java
+++ src/main/java/com/example/TextBlock.java
@@ -10,4 +10,49 @@ public class TextBlock {
                 into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
                 passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""";
     }
+    
+    public static String getMinimalPomXml() {
+        return """
+            <?xml version="1.0" encoding="UTF-8"?>
+
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+            
+              <groupId>com.example</groupId>
+              <artifactId>java17</artifactId>
+              <version>1.0-SNAPSHOT</version>
+            </project>""";
+    }
+    
+    public static String getActuatorBeansJSon() {
+        return """
+            {
+              "contexts" : {
+                "application" : {
+                  "beans" : {
+                    "org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration$DispatcherServletRegistrationConfiguration" : {
+                      "aliases" : [ ],
+                      "scope" : "singleton",
+                      "type" : "org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration$DispatcherServletRegistrationConfiguration",
+                      "dependencies" : [ ]
+                    },
+                    "org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration" : {
+                      "aliases" : [ ],
+                      "scope" : "singleton",
+                      "type" : "org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration",
+                      "dependencies" : [ ]
+                    },
+                    "org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration" : {
+                      "aliases" : [ ],
+                      "scope" : "singleton",
+                      "type" : "org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration",
+                      "dependencies" : [ ]
+                    }
+                  }
+                }
+              }
+            }""";
+    }
+    
 }
