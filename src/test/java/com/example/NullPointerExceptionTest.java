166798b7546ad3a4a29668b0f7a84a875ce5e345
Johan Janssen <johanjanssen2001@gmail.com>
2022-04-01 14:01:24 +0200

    Using textblocks for NullPointerException message.

--- src/test/java/com/example/NullPointerExceptionTest.java
+++ src/test/java/com/example/NullPointerExceptionTest.java
@@ -13,7 +13,9 @@ public class NullPointerExceptionTest {
         Exception expectedException = assertThrows(NullPointerException.class, () ->
                 training.studentList().get(0).lastName()
         );
-        assertEquals("Cannot invoke \"java.util.List.get(int)\" because the return value of \"com.example.Training.studentList()\" is null", expectedException.getMessage());
+        String expectedExceptionMessage = """
+                Cannot invoke "java.util.List.get(int)" because the return value of "com.example.Training.studentList()" is null""";
+        assertEquals(expectedExceptionMessage, expectedException.getMessage());
     }
 
     @Test
@@ -23,6 +25,8 @@ public class NullPointerExceptionTest {
         Exception expectedException = assertThrows(NullPointerException.class, () ->
                 nullTraining.studentList().get(0).lastName()
         );
-        assertEquals("Cannot invoke \"com.example.Training.studentList()\" because \"nullTraining\" is null", expectedException.getMessage());
+        String expectedExceptionMessage = """
+                Cannot invoke "com.example.Training.studentList()" because "nullTraining" is null""";
+        assertEquals(expectedExceptionMessage, expectedException.getMessage());
     }
 }
