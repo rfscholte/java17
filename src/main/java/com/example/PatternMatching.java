7c2dbf2c3b538afc21d7328298f48a2b1890ede6
Robert Scholte <rfscholte@apache.org>
2023-11-01 11:46:16 +0100

    Improve case when example

--- src/main/java/com/example/PatternMatching.java
+++ src/main/java/com/example/PatternMatching.java
@@ -4,13 +4,14 @@ public class PatternMatching {
 
     static String describeCard(CardClassification c) {
         return switch (c) {
-            case Suit s when s == Suit.CLUBS ->     "It's clubs"; // notice the 'when'
+            case Suit s
+                when s.name().startsWith("C") ->    "It's clubs"; // notice the 'when'
             case Suit.DIAMONDS ->                   { String d = "diamonds";
                                                       yield "It's " + d; }
             case Suit.HEARTS ->                     "It's hearts";
             case Suit s ->                          "It's spades"; // all others, like default, BAD!!
             case Tarot t ->                         "It's a tarot";
-            case null ->                            "You idiot";
+            case null /*, default */ ->             "You idiot";
         };
     }
 }
